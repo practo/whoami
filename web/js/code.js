9d33817024affb70c776036bd2a36f5ace35a00e
Siddhartha Nihalani <narenana@gmail.com>
2013-09-14 22:50:16 +0530

    added logged in file

---
+++ web/js/code.js
@@ -0,0 +1,21 @@
+$(function(){
+	if(localStorage){
+		if(!localStorage.whoami){
+			window.location = "/index.html";
+		}
+		else{
+			if(!localStorage.whoami.user.token){
+				window.location = "/index.html";
+			}
+		}
+	}
+	else{
+		alert("Sorry. You cant use this app.")
+	}
+
+	console.log(localStorage.whoami.user);
+
+
+
+	
+})
