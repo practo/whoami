2cec01ef3034beddf7fd92278036a2e8076318b3
Siddhartha Nihalani <narenana@gmail.com>
2013-09-14 21:18:29 +0530

    added signup

--- web/js/main.js
+++ web/js/main.js
@@ -6,7 +6,8 @@ $(function(){
 		e.preventDefault();
 
 		$.ajax({
-			url: host + "/user/auth",
+			url: host + "/users/authenticates",
+			type:"POST",
 			data:$("#signinform").serialize(),
 			success:function(response){
 				log(response);
@@ -17,132 +18,156 @@ $(function(){
 		})
 
 	})
+	$("#signupform button").click(function(e){
+		e.preventDefault();
 
-	FusionCharts.setCurrentRenderer('javascript');
+		$.ajax({
+			url: host + "/users",
+			type:"POST",
+			data:$("#signinform").serialize(),
+			success:function(response){
+				log(response);
+			},
+			error:function(){
+				alert("Sorry. Could not log you in.")
+			}
+		})
 
-	chartObject1 = new FusionCharts({
-            swfUrl: "Charts/Doughnut2D.swf",
-            width: "100%",
-            height: "350px"
-    });
-    chartObject2 = new FusionCharts({
-            swfUrl: "Charts/Doughnut2D.swf",
-            width: "100%",
-            height: "350px"
-    });
-    chartObject3 = new FusionCharts({
-            swfUrl: "Charts/Doughnut2D.swf",
-            width: "100%",
-            height: "350px"
-    });
+	})	
 
-    chartObject4 = new FusionCharts({
-            swfUrl: "Charts/Doughnut2D.swf",
-            width: "100%",
-            height: "350px"
-    });
+	if($("#chart1").length){
+		renderSampleCharts();
+	}
 
-    var chartData1 = {
-        "chart": {
-            "caption": "Where have you been ?",
-            "bgColor":"#ffffff",
-            "showvalues": "1",
-            "showlegend": "0",
-            "legendposition": "RIGHT",
-            slicingDistance: '25',
-            showBorder: "0",
-        },
-        data:[{label: "Work",
-                  value: 10},
-                { label: "Home",
-                  value: 20},
-                 {label: "Travel",
-                  value: 5},
-                  {label: "Other",
-                  value: 15}
-                  ]
-    };
-    var chartData2 = {
-        "chart": {
-            "caption": "What do I do at home ?",
-            "bgColor":"#ffffff",
-            "showvalues": "1",
-            "showlegend": "0",
-            "legendposition": "RIGHT",
-            slicingDistance: '25',
-            showBorder: "0",
-        },
-        data:[{label: "Facebook",
-                  value: 15},
-                { label: "Youtube",
-                  value: 20},
-                   { label: "Gaming",
-                  value: 20},
-                   { label: "Chat",
-                  value: 20},
-                   { label: "Productivity",
-                  value: 20},
-                 {label: "Sleep",
-                  value: 200},
-                  {label: "Not tracked",
-                  value: 10}
-                  ]
-    };
-    var chartData3 = {
-        "chart": {
-            "caption": "What do I do at work ?",
-            "bgColor":"#ffffff",
-            "showvalues": "1",
-            "showlegend": "0",
-            "legendposition": "RIGHT",
-            slicingDistance: '25',
-            showBorder: "0",
-        },
-        data:[{label: "Excel",
-                  value:60},
-                { label: "Powerpoint",
-                  value: 90},
-                 {label: "Mail",
-                  value: 50},
-                  {label: "Other",
-                  value: 15}
-                  ]
-    };
+	function renderSampleCharts(){
+		FusionCharts.setCurrentRenderer('javascript');
 
-    var chartData4 = {
-        "chart": {
-            "caption": "",
-            "bgColor":"#ffffff",
-            "bgAlpha":"0,0",
-            "showvalues": "1",
-            "showlegend": "0",
-            "legendposition": "RIGHT",
-            slicingDistance: '25',
-            showBorder: "0",
-        },
-        data:[{label: "Productivity",
-                  value:90},
-                { label: "Entertainment",
-                  value: 90},
-                 {label: "Sleep",
-                  value: 50},
-                  {label: "Travel",
-                  value: 25},{
-              	  label: "Networking",
-                  value: 15}
-                  ]
-    };
+		chartObject1 = new FusionCharts({
+	            swfUrl: "Charts/Doughnut2D.swf",
+	            width: "100%",
+	            height: "350px"
+	    });
+	    chartObject2 = new FusionCharts({
+	            swfUrl: "Charts/Doughnut2D.swf",
+	            width: "100%",
+	            height: "350px"
+	    });
+	    chartObject3 = new FusionCharts({
+	            swfUrl: "Charts/Doughnut2D.swf",
+	            width: "100%",
+	            height: "350px"
+	    });
+
+	    chartObject4 = new FusionCharts({
+	            swfUrl: "Charts/Doughnut2D.swf",
+	            width: "100%",
+	            height: "350px"
+	    });
+
+	    var chartData1 = {
+	        "chart": {
+	            "caption": "Where have you been ?",
+	            "bgColor":"#ffffff",
+	            "showvalues": "1",
+	            "showlegend": "0",
+	            "legendposition": "RIGHT",
+	            slicingDistance: '25',
+	            showBorder: "0",
+	        },
+	        data:[{label: "Work",
+	                  value: 10},
+	                { label: "Home",
+	                  value: 20},
+	                 {label: "Travel",
+	                  value: 5},
+	                  {label: "Other",
+	                  value: 15}
+	                  ]
+	    };
+	    var chartData2 = {
+	        "chart": {
+	            "caption": "What do I do at home ?",
+	            "bgColor":"#ffffff",
+	            "showvalues": "1",
+	            "showlegend": "0",
+	            "legendposition": "RIGHT",
+	            slicingDistance: '25',
+	            showBorder: "0",
+	        },
+	        data:[{label: "Facebook",
+	                  value: 15},
+	                { label: "Youtube",
+	                  value: 20},
+	                   { label: "Gaming",
+	                  value: 20},
+	                   { label: "Chat",
+	                  value: 20},
+	                   { label: "Productivity",
+	                  value: 20},
+	                 {label: "Sleep",
+	                  value: 200},
+	                  {label: "Not tracked",
+	                  value: 10}
+	                  ]
+	    };
+	    var chartData3 = {
+	        "chart": {
+	            "caption": "What do I do at work ?",
+	            "bgColor":"#ffffff",
+	            "showvalues": "1",
+	            "showlegend": "0",
+	            "legendposition": "RIGHT",
+	            slicingDistance: '25',
+	            showBorder: "0",
+	        },
+	        data:[{label: "Excel",
+	                  value:60},
+	                { label: "Powerpoint",
+	                  value: 90},
+	                 {label: "Mail",
+	                  value: 50},
+	                  {label: "Other",
+	                  value: 15}
+	                  ]
+	    };
+
+	    var chartData4 = {
+	        "chart": {
+	            "caption": "",
+	            "bgColor":"#ffffff",
+	            "bgAlpha":"0,0",
+	            "showvalues": "1",
+	            "showlegend": "0",
+	            "legendposition": "RIGHT",
+	            slicingDistance: '25',
+	            showBorder: "0",
+	        },
+	        data:[{label: "Productivity",
+	                  value:90},
+	                { label: "Entertainment",
+	                  value: 90},
+	                 {label: "Sleep",
+	                  value: 50},
+	                  {label: "Travel",
+	                  value: 25},{
+	              	  label: "Networking",
+	                  value: 15}
+	                  ]
+	    };
 
 
-    chartObject1.setJSONData(chartData1);
-    chartObject2.setJSONData(chartData2);
-    chartObject3.setJSONData(chartData3);
-    chartObject4.setTransparent(true);
-    chartObject4.setJSONData(chartData4);
+	    chartObject1.setJSONData(chartData1);
+	    chartObject2.setJSONData(chartData2);
+	    chartObject3.setJSONData(chartData3);
+	    chartObject4.setTransparent(true);
+	    chartObject4.setJSONData(chartData4);
 
-    chartObject1.render("chart1");
-    chartObject2.render("chart2");
-    chartObject3.render("chart3");
-    chartObject4.render("chart4");
+	    chartObject1.render("chart1");
+	    chartObject2.render("chart2");
+	    chartObject3.render("chart3");
+	    chartObject4.render("chart4");
+	}
+
+	
 
 })
